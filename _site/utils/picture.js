b10cff3bb72bea5e28fd57614efcb2b99cb53666
Iago Barreiro <iago.barreiro@gmail.com>
2020-08-24 13:48:23 +0200

    images changed

--- _site/utils/picture.js
+++ _site/utils/picture.js
@@ -4,15 +4,11 @@
 module.exports = (url, alt = "Missing alt text",picClass,lazy = false) => {
   if (lazy !== 'lazy') {
     return `<picture class="${picClass}">
-    <source srcset="/images/${url}?nf_resize=fit&w=700" media="(min-width: 1200px)">
-    <source srcset="/images/${url}?nf_resize=fit&w=600" media="(min-width: 740px)">
-    <img src="/images/${url}?nf_resize=fit&w=500" alt="${alt}" />
+    <img src="/images/${url}" alt="${alt}" />
   </picture>`;
 } else {
   return `<picture>
-  <source data-srcset="/images/${url}?nf_resize=fit&w=700" media="(min-width: 1200px)">
-  <source data-srcset="/images/${url}?nf_resize=fit&w=600" media="(min-width: 740px)">
-  <img data-src="/images/${url}?nf_resize=fit&w=500" alt="${alt}" />
+    <img src="/images/${url}" alt="${alt}" />
 </picture>`;
 }
 
